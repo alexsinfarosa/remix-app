c42474428fe2ef3a4d35aed5fb50d8097ad2c587
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-10-07 15:18:41 +0200

    refactor

--- app/routes/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { LoaderArgs } from "@remix-run/node";
-
-export async function loader(request: LoaderArgs) {
-  return {};
-}
-
-export default function AdminRoute() {
-  return <div>This is the admin route</div>;
-}
