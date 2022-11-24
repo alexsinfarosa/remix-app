cc6e030169c5ba506cd814dbf1e68c8b1dfbe9e7
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-11-24 11:08:31 +0100

    improve accessibility

--- app/routes/subscribe.tsx
+++ app/routes/subscribe.tsx
@@ -1,6 +1,6 @@
-import { Form, Link, useActionData, useTransition } from "@remix-run/react";
 import type { ActionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { Form, Link, useActionData, useTransition } from "@remix-run/react";
+import { useEffect, useRef } from "react";
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
@@ -34,6 +34,26 @@ export default function Subscribe() {
       ? "error"
       : "idle";
 
+  let inputRef = useRef<HTMLInputElement | null>(null);
+  let successRef = useRef<HTMLHeadingElement | null>(null);
+  let mounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (state === "error") {
+      inputRef.current?.focus();
+    }
+
+    if (state === "idle" && mounted.current) {
+      inputRef.current?.select();
+    }
+
+    if (state === "success") {
+      successRef.current?.focus();
+    }
+
+    mounted.current = true;
+  }, [state]);
+
   return (
     <main className="flex h-full items-center justify-center  text-blue-900">
       <section className="rounded-lg bg-blue-100 p-8">
@@ -50,12 +70,16 @@ export default function Subscribe() {
           Don't miss any action!
         </p>
         <Form
+          replace
           method="post"
           aria-hidden={state === "success"}
           className="aria-hidden:hidden"
         >
-          <fieldset className="space-x-2">
+          <fieldset className="space-x-2" disabled={state === "submitting"}>
             <input
+              aria-label="Email address"
+              aria-describedby="email-helper-text"
+              ref={inputRef}
               type="email"
               name="email"
               id="email"
@@ -64,13 +88,14 @@ export default function Subscribe() {
             />
             <button
               type="submit"
-              className="rounded-md border border-blue-600 px-3 py-2 hover:bg-blue-600 hover:text-white"
+              className="w-32 rounded-md border border-blue-600 px-3 py-2 hover:bg-blue-600 hover:text-white"
             >
               {state === "submitting" ? "Submitting..." : "Subscribe"}
             </button>
           </fieldset>
-          <p className=" mt-1 text-center">
-            {actionData?.error ? actionData.message : <>&nbsp;</>}
+
+          <p className="mt-4 text-center" id="email-helper-text">
+            {state === "error" ? actionData.message : <>&nbsp;</>}
           </p>
         </Form>
 
@@ -78,7 +103,13 @@ export default function Subscribe() {
           className="text-center aria-hidden:hidden"
           aria-hidden={state !== "success"}
         >
-          <h2 className="mb-1 text-3xl font-bold">You are subscribed!</h2>
+          <h2
+            className="mb-1 text-3xl font-bold"
+            ref={successRef}
+            tabIndex={-1}
+          >
+            You are subscribed!
+          </h2>
           <p>Please check your email to confirm subscription.</p>
           <Link to="." className="mt-1 underline">
             ← Go Back
